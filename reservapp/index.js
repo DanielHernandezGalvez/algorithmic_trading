73861d7879c2c0f76f712a123e2252e140654d15
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-02-23 17:54:26 -0600

    conectado a mongo db

--- reservapp/index.js
+++ reservapp/index.js
@@ -1,7 +1,32 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
+
+dotenv.config()
 
 const app = express();
 
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("CONECTADOS A MONGOdb")
+    } catch (error) {
+        throw error;
+    }
+}
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB conectado")
+})
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB desconectado")
+})
+
 app.listen(8800, () => {
+    connect()
     console.log("Conectado al backend");
 })
