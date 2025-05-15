d257c06ae6a164170f37ec2c327a0878df60d25d
unknown <hernandezgalvezalejandro@gmail.com>
2025-05-14 19:51:01 -0600

    change title on topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const TopBar = () => {
     return (
         <header className='bg-slate-900 p-5 text center text-white'>
-            <h1>Modos de renderizado en Next Js</h1>
+            <h1>Dinz Kramer</h1>
           
         </header>
     )
