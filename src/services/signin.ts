08a2a67d2f47507b0b3d511e06cc018814c08eef
unknown <hernandezgalvezalejandro@gmail.com>
2025-08-09 11:57:30 -0600

    traiding init

--- src/services/signin.ts
+++
@@ -1,7 +0,0 @@
-export const signinMock = async (email: string, password: string) => {
-    if(email === "daniel@vuelostudios.com" && password === "12345") {
-        return true
-    } else {
-        return false
-    }
-}
